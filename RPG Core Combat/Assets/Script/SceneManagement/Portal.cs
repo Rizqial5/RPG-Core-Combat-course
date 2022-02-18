554ca07eb5ea9728056471bd64d54bfaf8ea7927
Rizqial5 <bagusriski1@gmail.com>
2022-02-18 19:31:13 +0700

    Add new Scene
    - Add portal feature

---
+++ RPG Core Combat/Assets/Script/SceneManagement/Portal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int StageScene;
+        private void OnTriggerEnter(Collider other) {
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene(StageScene);
+            }
+        }
+    }
+
+}
